cc0094861a8941d143e0216007189267cb97f93a
Adriano Correa <86089849+Adriano-B-C@users.noreply.github.com>
2022-11-02 04:03:27 -0300

    Delete 4.js

--- 4.js
+++
@@ -1,9 +0,0 @@
-//  CERTO
-
-const divisão = function (dividendo, divisor) {
-  console.log(`Resultado: ${dividendo / divisor}`)
-  console.log(`Resultado: ${dividendo % divisor}`)
-}
-
-divisão(10, 2)
-divisão(10, 3)
